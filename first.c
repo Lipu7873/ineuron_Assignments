8502cd30b473e7e39f39e02b2e0d50264da4d6cb
Lipu <swainlipun1@gmail.com>
2022-10-12 23:57:03 +0530

    first

---
+++ first.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char a;
+    printf("hii lipu! how are you");
+    printf("hello Lipu Enter a charecter");
+    printf("ok sir");
+    a=getch();
+    printf("ASCII of %c is %d",a,a);
+return 0;
+}
