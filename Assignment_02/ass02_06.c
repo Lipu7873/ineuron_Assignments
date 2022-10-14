f90b0ba4ec1303d70087e23a3befc740acc20ed1
Lipu Swain <94134660+Lipu7873@users.noreply.github.com>
2022-10-14 09:10:56 +0530

    Create ass02_06.c

---
+++ Assignment_02/ass02_06.c
@@ -0,0 +1,10 @@
+// 6. Write a program which takes a character as an input and displays its ASCII code.
+#include<stdio.h>
+int main()
+{
+    char a;
+    printf("Enter a charectar: ");
+    scanf("%c",&a);
+    printf("The ASCII code of %c is %d",a,a);
+    return 0;
+}
