77412631babaed3a691cd7b5ce53e0cf2513f47c
Lipu Swain <94134660+Lipu7873@users.noreply.github.com>
2022-10-14 09:12:33 +0530

    Create ass02_09.c

---
+++ Assignment_02/ass02_09.c
@@ -0,0 +1,15 @@
+// 9. Write a program to print size of an int, a float, a char and a double type variable
+#include<stdio.h>
+int main()
+{
+    int a;
+    a=sizeof(int);
+    printf("size of int is %d",a);
+      a=sizeof(float);
+    printf("\nsize of int is %d",a);
+      a=sizeof(char);
+    printf("\nsize of int is %d",a);
+      a=sizeof(double);
+    printf("\nsize of int is %d",a);
+    return 0;
+}
