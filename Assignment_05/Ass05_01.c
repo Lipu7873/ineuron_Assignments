c0930b5a49bdce796b230d8d3888a44e2edf1bed
Lipu Swain <94134660+Lipu7873@users.noreply.github.com>
2022-10-31 02:56:42 +0530

    Create Ass05_01.c

---
+++ Assignment_05/Ass05_01.c
@@ -0,0 +1,11 @@
+/*1. Write a program to print MySirG N times on the screen*/
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    printf("Enter a no: ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    printf("MySirG\n");
+    return 0;
+}
