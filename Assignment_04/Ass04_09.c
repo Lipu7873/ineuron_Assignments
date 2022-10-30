ee99cf4b1c220bee6aa6d3a4ff9c7f613be2db94
Lipu Swain <94134660+Lipu7873@users.noreply.github.com>
2022-10-31 02:53:34 +0530

    Add files via upload

---
+++ Assignment_04/Ass04_09.c
@@ -0,0 +1,9 @@
+/*9. Write a program to print cubes of the first 10 natural numbers*/
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    printf("%d ",i*3);
+    return 0;
+}
---
+++ Assignment_04/Ass04_10.c
@@ -0,0 +1,9 @@
+/*10. Write a program to print a table of 5.*/
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    printf("%d ",i*5);
+    return 0;
+}
