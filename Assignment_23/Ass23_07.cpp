c8aa285a646157fc50ca97806528e305f6831a81
Lipu Swain <94134660+Lipu7873@users.noreply.github.com>
2022-11-06 23:39:16 +0530

    Create Ass23_07.cpp

---
+++ Assignment_23/Ass23_07.cpp
@@ -0,0 +1,11 @@
+/* 7. Write a C++ program to calculate the square of a number */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a no: ";
+    cin>>n;
+    cout<<"square of "<<n<<" is "<<n*n;
+    return 0;
+}
